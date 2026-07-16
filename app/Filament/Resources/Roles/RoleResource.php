c7b01f705ead95a48e82b62e2ae2be36bfd4c7c5
vlayd <vlaydissonu@gmail.com>
2026-07-15 23:15:06 -0500

    Iniciando um tabela custom de role

---
+++ app/Filament/Resources/Roles/RoleResource.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Filament\Resources\Roles;
+
+use App\Filament\Resources\Roles\Pages\CreateRole;
+use App\Filament\Resources\Roles\Pages\EditRole;
+use App\Filament\Resources\Roles\Pages\ListRoles;
+use App\Filament\Resources\Roles\Schemas\RoleForm;
+use App\Filament\Resources\Roles\Tables\RolesTable;
+use App\Models\Role;
+use BackedEnum;
+use BezhanSalleh\FilamentShield\Resources\Roles\RoleResource as RolesRoleResource;
+use Filament\Resources\Resource;
+use Filament\Schemas\Schema;
+use Filament\Support\Icons\Heroicon;
+use Filament\Tables\Table;
+
+class RoleResource extends RolesRoleResource
+{
+    public static function table(Table $table): Table
+    {
+        return RolesTable::configure($table);
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => ListRoles::route('/'),
+            'create' => CreateRole::route('/create'),
+            'edit' => EditRole::route('/{record}/edit'),
+        ];
+    }
+}
